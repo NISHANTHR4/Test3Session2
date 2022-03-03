0cf09938666500facee0894d1509097553437dc3
NISHANTHR4 <97089406+NISHANTHR4@users.noreply.github.com>
2022-03-03 14:15:13 +0000

    p1debug

--- p1final.c
+++ p1final.c
@@ -8,11 +8,11 @@ void add(int num1,int den1,int num2,int den2,int *num3,int *den3)
 {
   *num3= (num1*den2)+(num2*den1);
   *den3=den1*den2;
-  
+
 }
 void output(int num1, int den1, int num2, int den2, int num3, int den3)
 {
-  printf("%d/%d + %d/%d = %d/%d",num1,den1,num2,den2,num3,den3);
+  printf("%d/%d + %d/%d = %d/%d ",num1,den1,num2,den2,num3,den3);
   }
 void main()
 {
